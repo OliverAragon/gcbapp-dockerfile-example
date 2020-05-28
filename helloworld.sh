dd79f943b29dfcf78ec3fc683db8485f75fc7389
Oliver Aragon <42587820+OliverAragon@users.noreply.github.com>
2020-05-28 17:14:37 -0500

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Hello, world, this is Ara!"
+echo "Hello, world, this is Ara, version 2!"
 echo "The time is $(date)."
